46015e2c668c0beac194993c2855582055c7e75a
HxDxRx <89873868+HxDxRx@users.noreply.github.com>
2025-01-22 20:18:28 -0600

    Create ogg_to_mp4.bat
    Quickly extract the disc art from zotify ogg files and convert them to H.264 (AVC) mp4 video files for playback on simpler devices

---
+++ ogg_to_mp4.bat
@@ -0,0 +1,15 @@
+@echo off
+for /R %%i in (*.ogg) do (
+    echo Processing "%%i"
+    
+    :: Extract the disc art
+    ffmpeg -i "%%i" -an -vcodec copy cover.jpg
+    
+    :: Create the final MP4 with disc art and converted audio
+    :: -CRF is set high because it's a still image
+    ffmpeg -loop 1 -i cover.jpg -i "%%i" -c:v libx264 -pix_fmt yuv420p -crf 35 -c:a aac -strict experimental -shortest "%%~dpni.mp4"
+    
+    :: Clean up temporary files
+    del cover.jpg
+)
+pause
