84e7ac5579d6e1a4444469b77f0fcc4522493776
HxDxRx <89873868+HxDxRx@users.noreply.github.com>
2025-01-22 20:40:44 -0600

    Update ogg_to_mp4.bat
    Use variable bitrate, not default 128k

--- ogg_to_mp4.bat
+++ ogg_to_mp4.bat
@@ -7,7 +7,7 @@ for /R %%i in (*.ogg) do (
     
     :: Create the final MP4 with disc art and converted audio
     :: -CRF is set high because it's a still image
-    ffmpeg -loop 1 -i cover.jpg -i "%%i" -c:v libx264 -pix_fmt yuv420p -crf 35 -c:a aac -strict experimental -shortest "%%~dpni.mp4"
+    ffmpeg -loop 1 -i cover.jpg -i "%%i" -c:v libx264 -pix_fmt yuv420p -crf 35 -c:a aac -q:a 0 -strict experimental -shortest "%%~dpni.mp4"
     
     :: Clean up temporary files
     del cover.jpg
